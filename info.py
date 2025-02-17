9d20b02748666838f7aa6827284df85024a62de5
Farook13 <umarulfarook787@gmail.com>
2025-02-17 22:32:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ def is_enabled(value, default):
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '21484'))
-API_HASH = environ.get('API_HASH', 'e7c6c1e727962d2ade50bald7f4fac8a')
+API_HASH = environ.get('API_HASH', 'db6286a041adf07ef4244e')
 BOT_TOKEN = environ.get('BOT_TOKEN', '7955983025:AAFqrIti8CrhCvPTY0IKV2qu4xnxF96sL40')
 PORT = environ.get("PORT", "8080")
 
